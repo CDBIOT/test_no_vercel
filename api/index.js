4c84ca1f0b955314ec40fe7c84de238b8244b58f
Cristiano Bento <crisdb2008@gmail.com>
2022-12-30 16:49:14 -0300

    second commit

---
+++ api/index.js
@@ -0,0 +1,22 @@
+
+const { Router } = 'express';
+import express from 'express';
+const app = express();
+
+
+//Read
+app.get('/', async (req, res) =>{
+    try{
+        await Person.create(person)
+        res.status(201).json({message: "Pessoa inserida com sucesso"})
+    }catch(error){
+        res.status(500).json({error: error})
+    }  
+})
+
+
+const PORT = process.env.PORT || 4000 || 5500;
+    app.listen(PORT,function(){
+        console.log("Servidor Rodando" + 4000);
+        })
+
