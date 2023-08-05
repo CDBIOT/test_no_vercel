e6c58a7a3d3324c586a1672f03a78c1ca3f578e1
Cristiano Bento <crisdb2008@gmail.com>
2023-08-05 16:42:40 -0300

    db

--- db_atlas.js
+++ db_atlas.js
@@ -1,5 +1,16 @@
 if(process.env.NODE_ENV == "production"){
 
+//     module.exports = 
+//    {
+    const MONGODB_URI = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0.mvho6.mongodb.net/'
+    +process.env.DB_NAME+'?retryWrites=true&w=majority'
+    //},
+  // {
+   // useNewUrlParser: true,
+    //useUnifiedTopology: true
+  //  },
+ //   }
+
 module.exports = {MONGODB_URI: "mongodb+srv://cdb:abcdeF12345@cluster0.mvho6.mongodb.net/equipamentos"},
 //module.exports = {URI: "https://polar-beyond-82520.herokuapp.com/"},
 {
