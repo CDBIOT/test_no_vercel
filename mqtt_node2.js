d4bf1d3e125b56152dd636ef14ddb3a2b629173e
CDBIOT <crisdb2008@gmail.com>
2023-09-27 16:20:41 -0300

    te

--- mqtt_node2.js
+++ mqtt_node2.js
@@ -39,26 +39,26 @@ client.on('message', (topic, payload) => {
   
 }, 1000);
 
-const topic3 = 'Quarto'
-client.on('connect', () => {
-  console.log('Connected on topic Quarto')
-  client.subscribe([topic3], () => {
-    console.log(`Subscribe to topic '${topic3}'`)
+// const topic3 = 'Quarto'
+// client.on('connect', () => {
+//   console.log('Connected on topic Quarto')
+//   client.subscribe([topic3], () => {
+//     console.log(`Subscribe to topic '${topic3}'`)
 
-client.on('message', (topic3, payload) => {
-      temp3 = payload
-      local3= topic3
-     console.log('Received Message:', local3, payload.toString())
-      //res.status(200).json({m})
-    })
-  })
-})
+// client.on('message', (topic3, payload) => {
+//       temp3 = payload
+//       local3= topic3
+//      console.log('Received Message:', local3, payload.toString())
+//       //res.status(200).json({m})
+//     })
+//   })
+// })
 
- client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: true }, (error) => {
- if (error) {
-     console.error(error)
-   }
- })
+//  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: true }, (error) => {
+//  if (error) {
+//      console.error(error)
+//    }
+//  })
 
 const topic2 = 'Teste1'
 client.on('connect', () => {
@@ -67,7 +67,7 @@ client.on('connect', () => {
     console.log(`Subscribe to topic '${topic2}'`)
 
 client.on('message', (topic2, payload) => {
-      temp2 = payload
+      temp2 = payload.toString()
       local2= topic2
      console.log('Received Aqua Message:', local2, payload.toString())
      // res.status(200).json({m})
