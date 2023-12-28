3123360cd4e1662335d9c1e3a730d6606665fac9
CDBIOT <cristiab05@yahoo.com.br>
2023-12-28 18:13:02 -0300

    1.0.7

--- api/index.js
+++ api/index.js
@@ -35,12 +35,9 @@ app.get('/mqtt2', rotas_temps.getMqtt2)
 app.post('/temps', rotas_temps.postTemps)
 app.delete('/temps',rotas_temps.deleteTemp)
 
-routers.get('/mqtt_on', mqtt.onLight)
+app.get('/mqtt_on', mqtt.onLight)
 
-routers.get('/mqtt_off', mqtt.offLight)
-
-
-
+app.get('/mqtt_off', mqtt.offLight)
 
 
 app.get ('/user',rotas_user.getUser)
