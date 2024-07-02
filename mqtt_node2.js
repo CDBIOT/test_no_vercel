a664bd54735a1beb4e0b4bc783fc43b61672ca37
CDBIOT <cristiab05@yahoo.com.br>
2024-07-02 12:02:01 -0300

    1

--- mqtt_node2.js
+++ mqtt_node2.js
@@ -29,7 +29,7 @@ client.on('connect', function () {
 
     console.log('Subscribe to topic Room_light via mqtt2')
     if (!err) {
-      client.publish('room_light', '0')
+      client.publish('room_light', '1')
     }
   })
   client.end()
