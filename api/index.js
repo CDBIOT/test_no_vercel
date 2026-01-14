39fbc4a65147a36582ce3c5106ea4609492155c8
CDBIOT <cristiab05@yahoo.com.br>
2026-01-14 19:46:34 -0300

    mqtt

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) =>{
 
 app.get('/mqtt',mqtt.mqtt)
 app.get('/temps', rotas_temps.getTemps)
-app.get('/mqtt',rotas_temps.getMqtt)
+//app.get('/mqtt',rotas_temps.getMqtt)
 app.get('/mqtt2', rotas_temps.getMqtt2)
 app.post('/temps', rotas_temps.postTemps)
 app.delete('/temps',rotas_temps.deleteTemp)
